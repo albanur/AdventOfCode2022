4423a0adfd5f6e78a381a71929863cadffe31f07
Grzegorz Kossowski <kossowskig@gmail.com>
2022-12-28 00:58:50 +0100

    Solution for Day 16

--- Day16/main.cpp
+++ Day16/main.cpp
@@ -37,11 +37,31 @@ struct State
 
 struct Worker
 {
+    int currentRoom;
     int destinationRoom;
     int timeLeft;
 
-    Worker() : destinationRoom(0), timeLeft(0) {}
-    Worker(int destination, int cost) : destinationRoom(destination), timeLeft(cost) { }
+    Worker() : currentRoom(0), destinationRoom(0), timeLeft(0) {}
+    Worker(int room) : currentRoom(room), destinationRoom(room), timeLeft(0) {}
+};
+
+struct State2
+{
+    int time;
+    set<int> roomsToVisit;
+    int flowPerMinute;
+    int releasedPressure;
+    Worker worker1;
+    Worker worker2;
+
+    State2(int roomId)
+    {
+        time = 0;
+        flowPerMinute = 0;
+        releasedPressure = 0;
+        worker1 = Worker(roomId);
+        worker2 = Worker(roomId);
+    }
 };
 
 int DoubleLetterToId(const char* letters)
@@ -82,30 +102,6 @@ void FindBestPaths(map<int, Room*>& rooms)
             }
         }
     }
-}
-
-extern int DecideAndProceed(map<int, Room*>& rooms, State state, vector<Worker>* workers, int maxTime);
-
-int DecideAndProceed(map<int, Room*>& rooms, State state, vector<Worker>* workers, int maxTime)
-{
-    if(state.time == maxTime)
-    {
-        return state.releasedPressure;
-    }
-
-    for(int i = 0; i < workers->size(); i++)
-    {
-        
-    }
-}
-
-int FindMaxReleseadPressure(map<int, Room*>& rooms, int availableWorkers, int maxTime)
-{
-    State state(DoubleLetterToId("AA"));
-    vector<Worker>* workers = new vector<Worker>(2, Worker());
-    int result = DecideAndProceed(rooms, state, workers, maxTime);
-    delete workers;
-    return result;
 }
 
 extern int DecideAndMove(map<int, Room*>& rooms, State state, int maxTime);
@@ -164,6 +160,255 @@ int MoveAndOpenValve(map<int, Room*>& rooms, State state, int roomId, int maxTim
     return DecideAndMove(rooms, state, maxTime);
 }
 
+extern int DecideAndMove2(map<int, Room*>& rooms, State2 state, int maxTime);
+extern int MoveAndOpenValve2(map<int, Room*>& rooms, State2 state, int maxTime);
+
+int FindMaxReleseadPressure2(map<int, Room*>& rooms, int maxTime)
+{
+    State2 state(DoubleLetterToId("AA"));
+    for(auto& kv : rooms)
+    {
+        if(kv.second->FlowRate > 0)
+        state.roomsToVisit.insert(kv.first);
+    }
+    return DecideAndMove2(rooms, state, maxTime);
+}
+
+inline int Score(map<int, Room*>& rooms, int remainingTime, int startRoom, int endRoom)
+{
+    int cost = rooms[startRoom]->BestPaths[endRoom] + 1;
+    if (cost >= remainingTime)
+    {
+        return 0;
+    }
+
+    return (remainingTime - cost) * rooms[endRoom]->FlowRate;
+}
+
+void GenerateSingleDecisions(map<int, Room*>& rooms, State2& state, int maxTime, int startRoom, vector<int>& decisions)
+{
+    int max = 0;
+    vector<int> all;
+
+    for(const auto& d : state.roomsToVisit)
+    {
+        int score = Score(rooms, maxTime - state.time, startRoom, d);
+        if (score > 0)
+        {
+            all.push_back(d);
+            if(score > max) { max = score; }
+        }
+    }
+
+    if (all.size() == 0)
+    {
+        decisions.push_back(-1);
+        return;
+    }
+
+    int threshold = max/2;
+    for(int d : all)
+    {
+        if (Score(rooms, maxTime - state.time, startRoom, d) >= threshold)
+        {
+            decisions.push_back(d);
+        }
+    }
+}
+
+void GenerateDoubleDecisions0(State2& state, vector<pair<int, int>>& decisions)
+{
+    if (state.roomsToVisit.size() == 0)
+    {
+        decisions.push_back(pair<int,int>(-1,-1));
+        return;
+    }
+    if (state.roomsToVisit.size() == 1)
+    {
+        decisions.push_back(pair<int,int>(*(state.roomsToVisit.begin()) ,-1));
+        return;
+    }
+
+    vector<int> d(state.roomsToVisit.begin(), state.roomsToVisit.end());
+
+    for(int i = 0; i < d.size(); i++)
+    {
+        for(int j = i+1; j < d.size(); j++)
+        {
+            decisions.push_back(pair<int,int>(d[i],d[j]));
+        }
+    }
+}
+
+void GenerateDoubleDecisions(map<int, Room*>& rooms, State2& state, int maxTime, vector<pair<int, int>>& decisions)
+{
+    if (state.roomsToVisit.size() == 0)
+    {
+        decisions.push_back(pair<int,int>(-1,-1));
+        return;
+    }
+    if (state.roomsToVisit.size() == 1)
+    {
+        decisions.push_back(pair<int,int>(*(state.roomsToVisit.begin()) ,-1));
+        return;
+    }
+
+    //vector<int> d(state.roomsToVisit.begin(), state.roomsToVisit.end());
+    vector<int> d1, d2;
+    GenerateSingleDecisions(rooms, state, maxTime, state.worker1.currentRoom, d1);
+    GenerateSingleDecisions(rooms, state, maxTime, state.worker2.currentRoom, d2);
+
+    for(int i = 0; i < d1.size(); i++)
+    {
+        for(int j = 0; j < d2.size(); j++)
+        {
+            if (d1[i] != d2[j])
+                decisions.push_back(pair<int,int>(d1[i],d2[j]));
+        }
+    }
+
+    if (decisions.size() == 0)
+    {
+        decisions.push_back(pair<int,int>(-1,-1));
+    }
+}
+
+int DecideAndMove2(map<int, Room*>& rooms, State2 state, int maxTime)
+{
+    if(state.time == maxTime)
+    {
+        return state.releasedPressure;
+    }
+
+    int max = 0;
+
+    if (state.worker1.destinationRoom == state.worker1.currentRoom
+        && state.worker2.destinationRoom == state.worker2.currentRoom)
+    {
+        vector<pair<int,int>> decisions;
+        if(state.time == 0)
+        {
+            GenerateDoubleDecisions0(state, decisions);
+        }
+        else
+        {
+            GenerateDoubleDecisions(rooms, state, maxTime, decisions);
+        }
+
+        for(auto& d : decisions)
+        {
+            if (d.first >= 0)
+            {
+                state.worker1.destinationRoom = d.first;
+                state.worker1.timeLeft = rooms[state.worker1.currentRoom]->BestPaths[state.worker1.destinationRoom] + 1;
+            }
+            else
+            {
+                state.worker1.destinationRoom = d.first;
+                state.worker1.timeLeft = maxTime + 1;
+            }
+
+            if (d.second >= 0)
+            {
+                state.worker2.destinationRoom = d.second;
+                state.worker2.timeLeft = rooms[state.worker2.currentRoom]->BestPaths[state.worker2.destinationRoom] + 1;
+            }
+            else
+            {
+                state.worker2.destinationRoom = d.second;
+                state.worker2.timeLeft = maxTime + 1;
+            }
+
+            state.roomsToVisit.erase(d.first);
+            state.roomsToVisit.erase(d.second);
+
+            int value = MoveAndOpenValve2(rooms, state, maxTime);
+            if(value > max) { max = value; }
+
+            if(d.first >= 0) { state.roomsToVisit.insert(d.first); }
+            if(d.second >= 0) { state.roomsToVisit.insert(d.second); }
+        }
+    }
+    else if (state.worker1.destinationRoom == state.worker1.currentRoom)
+    {
+        vector<int> decisions;
+        GenerateSingleDecisions(rooms, state, maxTime, state.worker1.currentRoom, decisions);
+
+        for(auto& d : decisions)
+        {
+            if (d >= 0)
+            {
+                state.worker1.destinationRoom = d;
+                state.worker1.timeLeft = rooms[state.worker1.currentRoom]->BestPaths[state.worker1.destinationRoom] + 1;
+            }
+            else
+            {
+                state.worker1.destinationRoom = d;
+                state.worker1.timeLeft = maxTime + 1;
+            }
+
+            state.roomsToVisit.erase(d);
+
+            int value = MoveAndOpenValve2(rooms, state, maxTime);
+            if(value > max) { max = value; }
+
+            if(d >= 0) { state.roomsToVisit.insert(d); }
+        }
+    }
+    else if (state.worker2.destinationRoom == state.worker2.currentRoom)
+    {
+        vector<int> decisions;
+        GenerateSingleDecisions(rooms, state, maxTime, state.worker2.currentRoom, decisions);
+
+        for(auto& d : decisions)
+        {
+            if (d >= 0)
+            {
+                state.worker2.destinationRoom = d;
+                state.worker2.timeLeft = rooms[state.worker2.currentRoom]->BestPaths[state.worker2.destinationRoom] + 1;
+            }
+            else
+            {
+                state.worker2.destinationRoom = d;
+                state.worker2.timeLeft = maxTime + 1;
+            }
+
+            state.roomsToVisit.erase(d);
+
+            int value = MoveAndOpenValve2(rooms, state, maxTime);
+            if(value > max) { max = value; }
+
+            if(d >= 0) { state.roomsToVisit.insert(d); }
+        }
+    }
+
+    return max;
+}
+
+int MoveAndOpenValve2(map<int, Room*>& rooms, State2 state, int maxTime)
+{
+    int time = min(state.worker1.timeLeft, state.worker2.timeLeft);
+    time = min(time, maxTime - state.time);
+
+    state.time += time;
+    state.releasedPressure += time * state.flowPerMinute;
+    state.worker1.timeLeft -= time;
+    state.worker2.timeLeft -= time;
+
+    if (state.worker1.timeLeft == 0)
+    {
+        state.worker1.currentRoom = state.worker1.destinationRoom;
+        state.flowPerMinute += rooms[state.worker1.destinationRoom]->FlowRate;
+    }
+    if (state.worker2.timeLeft == 0)
+    {
+        state.worker2.currentRoom = state.worker2.destinationRoom;
+        state.flowPerMinute += rooms[state.worker2.destinationRoom]->FlowRate;
+    }
+
+    return DecideAndMove2(rooms, state, maxTime);
+}
+
 int main()
 {
     char id[3];
@@ -190,7 +435,8 @@ int main()
 
     FindBestPaths(rooms);
 
-    int value = FindMaxReleseadPressure(rooms, 30);
+    //int value = FindMaxReleseadPressure(rooms, 30);
+    int value = FindMaxReleseadPressure2(rooms, 26);
 
     printf("%i\n", value);
 
